972b658dc2cae7c920658b859ffe3099f6a66e55
chen.zhang <zhangchen64d@gmail.com>
2021-01-07 14:34:11 +0900

    add env file

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+const { SNOWPACK_PUBLIC_API_URL } = import.meta.env;
+console.log('SNOWPACK_PUBLIC_API_URL..', SNOWPACK_PUBLIC_API_URL);
 
 ReactDOM.render(
   <React.StrictMode>
